5733cb4d843c7abe12fc3b3f7c7f396495d4e50a
parthgreycell <parthchauhan.egreycell@gmail.com>
2023-02-03 10:53:47 +0530

    Updated Jenkinsfile_ServiceTitan_ECR_Publish.groovy file

---
+++ Jenkinsfile_ServiceTitan_ECR_Publish.groovy
@@ -0,0 +1,120 @@
+node{
+  try{
+    properties([
+      authorizationMatrix([
+        'USER:com.cloudbees.plugins.credentials.CredentialsProvider.View:langyufei',
+        'USER:hudson.model.Item.Read:langyufei'
+      ]),
+      buildDiscarder(logRotator(numToKeepStr: '10')),
+      disableConcurrentBuilds(abortPrevious: false),
+      disableResume(),
+      parameters([
+        // choice(choices: ['dev', 'qa', 'uat', 'prod'], description: '', name: 'BuildProfile'),
+        choice(choices: ['dev'], description: '', name: 'BuildProfile'),
+        [$class: 'ListSubversionTagsParameterDefinition', credentialsId: 'munjal-gc-un-pw', defaultValue: '', maxTags: '', name: 'TagName', reverseByDate: true, reverseByName: false, tagsDir: 'https://github.com/BidClips/BidClips-ServiceTitan-API.git', tagsFilter: '']
+      ])
+    ])
+    def PUBLISHTAG = ""
+    def repoRegion = ""
+
+    stage('Preparation'){
+      if (''.equals(TagName)){
+        echo "[FAILURE] TagName parameter is required - Failed to build, value provided : ${TagName}"
+        currentBuild.result = 'FAILURE'
+        throw new RuntimeException("required parameter missing : ${TagName}");
+      }
+      dir('BidClips-ServiceTitan-API') {
+        if (TagName.startsWith('tags')) {
+          checkout poll: false, scm: [$class: 'GitSCM', branches: [[name: 'refs/${TagName}']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'munjal-gc', url: 'git@github.com:BidClips/BidClips-ServiceTitan-API.git']]]
+          PUBLISHTAG = TagName.split('/')[1]
+          repoRegion = "ap-southeast-1"
+        }
+        if (TagName.startsWith('branches')) {
+          def branch = TagName.split('/')[1]
+          checkout poll: false, scm: [$class: 'GitSCM', branches: [[name: branch]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'munjal-gc', url: 'git@github.com:BidClips/BidClips-ServiceTitan-API.git']]]
+          PUBLISHTAG = sh(
+            script: 'echo $(git log -1 --pretty=%h)',
+            returnStdout: true
+          ).trim()
+          repoRegion = "ap-southeast-1"
+        }
+        if (TagName.equals('trunk')) {
+          TagName = 'branches/master'
+          checkout poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'munjal-gc', url: 'git@github.com:BidClips/BidClips-ServiceTitan-API.git']]]
+          PUBLISHTAG = sh(
+            script: 'echo $(git log -1 --pretty=%h)',
+            returnStdout: true
+          ).trim()
+          repoRegion = "ap-southeast-1"
+
+        }
+      }
+    }
+
+    stage('Building Docker Image'){
+      dir('BidClips-ServiceTitan-API') {
+        def BUILDENV = ""
+        if(BuildProfile=="dev"){
+          BUILDENV="dev"
+        } else {
+          BUILDENV="prod"
+        }
+        sh """
+        echo "Build Profile : ${BUILDENV}"
+        sudo update-alternatives --set java /usr/lib/jvm/java-11-openjdk-11.0.11.0.9-1.amzn2.0.1.x86_64/bin/java
+        sudo update-alternatives --set javac /usr/lib/jvm/java-11-openjdk-11.0.11.0.9-1.amzn2.0.1.x86_64/bin/javac
+        ./gradlew bootJar -P${BUILDENV} jibDockerBuild
+        sudo update-alternatives --set java /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.282.b08-1.amzn2.0.1.x86_64/jre/bin/java
+        sudo update-alternatives --set javac /usr/lib/jvm/java-1.8.0-openjdk-1.8.0.282.b08-1.amzn2.0.1.x86_64/bin/javac
+        docker images | grep bidclipsservicetitanapi
+        """
+      }
+    }
+    stage("Publishing ${PUBLISHTAG}"){
+      sh """
+export AWS_PROFILE=bidclips-eks
+aws ecr get-login-password --region ${repoRegion} | docker login --username AWS --password-stdin 566570633830.dkr.ecr.${repoRegion}.amazonaws.com
+docker tag bidclipsservicetitanapi:latest 566570633830.dkr.ecr.${repoRegion}.amazonaws.com/bidclips-servicetitan-api:${PUBLISHTAG}
+docker push 566570633830.dkr.ecr.${repoRegion}.amazonaws.com/bidclips-servicetitan-api:${PUBLISHTAG}
+        """
+//       if(repoRegion == "us-east-1"){
+//         // us-east-1 means docker image built from tag; not branch
+//         // if so, push it to prod ecr as well, to avoid cross-tenant authorization pain
+//         sh """
+// export AWS_PROFILE=bidclips-prod-ecr
+// aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 875588116685.dkr.ecr.us-east-1.amazonaws.com
+// docker tag bidclipsservicetitanapi:latest 875588116685.dkr.ecr.us-east-1.amazonaws.com/bidclips-servicetitan-api:${PUBLISHTAG}
+// docker push 875588116685.dkr.ecr.us-east-1.amazonaws.com/bidclips-servicetitan-api:${PUBLISHTAG}
+// docker image rmi -f 875588116685.dkr.ecr.us-east-1.amazonaws.com/bidclips-servicetitan-api:${PUBLISHTAG}
+//         """
+//       }
+    }
+    stage('Cleanup'){
+      sh """
+      docker image rmi -f bidclipsservicetitanapi:latest
+      docker image rmi -f 566570633830.dkr.ecr.${repoRegion}.amazonaws.com/bidclips-servicetitan-api:${PUBLISHTAG}
+      """
+      dir('BidClips-ServiceTitan-API') {
+        deleteDir()
+      }
+    }
+  }
+  catch( exec ) {
+    echo "FAILURE: ${exec}"
+    currentBuild.result = 'FAILURE'
+  }
+
+  finally {
+    echo 'FINALLY BLOCK!'
+    cleanWs()
+    if (currentBuild.result == 'UNSTABLE') {
+      echo 'I am unstable :/'
+    }
+    else if (currentBuild.result == 'FAILURE'){
+      echo 'FAILURE!'
+    }
+    else {
+      echo 'One way or another, I have finished'
+    }
+  }
+}
